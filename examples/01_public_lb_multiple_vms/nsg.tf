25113dd8e6eda4356971d3349e71e998ee424364
mlinxfeld <martin.linxfeld@foggykitchen.com>
2026-02-04 18:08:23 +0100

    Updated examples with terraform-az-fk-nsg module usage instead of raw resources

--- examples/01_public_lb_multiple_vms/nsg.tf
+++ examples/01_public_lb_multiple_vms/nsg.tf
@@ -1,36 +1,40 @@
-resource "azurerm_network_security_group" "fk_subnet_private_nsg" {
+module "nsg" {
+  source = "github.com/mlinxfeld/terraform-az-fk-nsg"
+
   name                = "${var.vm_name}-nsg"
   location            = azurerm_resource_group.foggykitchen_rg.location
   resource_group_name = azurerm_resource_group.foggykitchen_rg.name
 
-  security_rule {
-    name                       = "allow-http-from-azure-lb"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "AzureLoadBalancer"
-    destination_address_prefix = "*"
-  }
+  rules = [
+    {
+      name                       = "allow-http-from-azure-lb"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "AzureLoadBalancer"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "allow-http-from-internet"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
 
-  security_rule {
-    name                       = "allow-http-from-internet"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"   
-    destination_address_prefix = "*"
+  subnet_associations = {
+    private_subnet = {
+      subnet_id = module.vnet.subnet_ids["fk-subnet-private"]
+    }
   }
 
   tags = var.tags
-}
-
-resource "azurerm_subnet_network_security_group_association" "fk_subnet_private_nsg_assoc" {
-  subnet_id                 = module.vnet.subnet_ids["fk-subnet-private"]
-  network_security_group_id = azurerm_network_security_group.fk_subnet_private_nsg.id
 }
--- examples/README.md
+++ examples/README.md
@@ -95,6 +95,7 @@ The goal is **clarity and correctness**, not completeness.
 
 - [terraform-az-fk-loadbalancer](https://github.com/mlinxfeld/terraform-az-fk-loadbalancer) (this repository)
 - [terraform-az-fk-compute](https://github.com/mlinxfeld/terraform-az-fk-compute)
+- [terraform-az-fk-nsg](https://github.com/mlinxfeld/terraform-az-fk-nsg)
 - [terraform-az-fk-vnet](https://github.com/mlinxfeld/terraform-az-fk-vnet)
 - [terraform-az-fk-aks](https://github.com/mlinxfeld/terraform-az-fk-aks)
 
